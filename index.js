efcb6f08637595214c186bcc2fc865ed329a914d
Steve Ledsworth <sledsworth@gmail.com>
2020-09-16 03:38:36 +0000

    intitial parser bot is mostly working! Finished nodemailer impleemntation to email on element being found

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const nodemailer = require("nodemailer");
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
@@ -8,11 +8,17 @@ const config = require('./config.json');
 async function fetchPage(url, query = config.default.query, options = config.options) {
 	const dom = await JSDOM.fromURL(url, options);
 	const element = dom.window.document.body.querySelector(query);
-	console.log(element);
 	if (element) {
-		console.log(`${url}: ${element.toString()} found!`);
 		notifyOfFind({
-			text: `${url}: ${element.toString()} found!`,
+			text: 
+			`
+${dom.window.document.title}
+
+${url}
+
+
+${element.outerHTML} found!
+			`,
 		});
 		return true;
 	}
@@ -30,15 +36,21 @@ async function notifyOfFind(details) {
 			pass: process.env.EMAIL_PASSWORD, // generated ethereal password
 		},
 	});
-
-	// send mail with defined transport object
-	let info = await transporter.sendMail({
-		from: config.notifications.from, // sender address
-		to: config.notifications.to, // list of receivers
-		subject: details.subject || config.notifications.subject, // Subject line
-		text: details.text, // plain text body
-		html: details.html, // html body
-	});
+	
+	let info;
+	try {
+		// send mail with defined transport object
+		info = await transporter.sendMail({
+			from: config.notifications.from, // sender address
+			to: config.notifications.to, // list of receivers
+			subject: details.subject || config.notifications.subject, // Subject line
+			text: details.text, // plain text body
+			html: details.html, // html body
+		});
+	} catch(e) {
+		console.log(e);
+		return;
+	}
 
 	console.log('Message sent: %s', info.messageId);
 	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
