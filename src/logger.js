5de13601f1f5086ced898339b91118e5ee5919a1
Steve Ledsworth <sledsworth@gmail.com>
2020-12-09 05:46:26 +0000

    factored out mailing to it’s own file. Added a proper logging library to help track down issues

---
+++ src/logger.js
@@ -0,0 +1,37 @@
+const winston = require("winston");
+
+const logConfiguration = {
+  exitOnError: false,
+  transports: [
+    new winston.transports.Console({
+      level: "silly",
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({
+      level: "info",
+      filename: "./logs/info.log",
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({
+      level: "error",
+      filename: "./logs/errors.log",
+    }),
+    new winston.transports.File({
+      filename: "./logs/exceptions.log",
+      handleExceptions: true,
+    }),
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({ filename: "./logs/rejections.log" }),
+  ],
+};
+
+const logger = winston.createLogger(logConfiguration);
+
+module.exports = logger;
---
+++ src/mailer.js
@@ -0,0 +1,41 @@
+const nodemailer = require("nodemailer");
+
+const config = require("../config.json");
+const logger = require("./logger.js");
+
+/**
+ *
+ * @param details { subject: string, html: string, text: string }
+ **/
+async function sendMail(details) {
+  let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  let info;
+  try {
+    // send mail with defined transport object
+    info = await transporter.sendMail({
+      from: config.notifications.from, // sender address
+      to: config.notifications.to, // list of receivers
+      subject: details.subject || config.notifications.subject, // Subject line
+      text: details.text, // plain text body
+      html: details.html, // html body
+    });
+  } catch (e) {
+    logger.error(`Node Mailer failed to send email with error: ${e}`);
+    return;
+  }
+
+  logger.info(`Message sent: ${info.messageId}`);
+}
+
+module.exports = {
+  sendMail,
+};
